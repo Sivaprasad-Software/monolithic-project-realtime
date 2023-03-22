eb620c1005d66325d12ff91ef65b8d862ecbd9cb
EC2 Default User <ec2-user@ip-172-31-13-182.eu-west-2.compute.internal>
2023-03-22 13:13:36 +0000

    all files

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my-vm" {
+ami = "ami-0055e70f580e9ae80"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.siva-sg.id]
+tags = {
+Name = "my-project-instance"
+Environment = "PROD"
+}
+key_name = "london"
+}
+
