6b55574495799334b08b3658968cf56516cbd538
Gitbysiva <118656180+Sivaprasad-Software@users.noreply.github.com>
2023-03-23 17:16:52 +0530

    update

--- terraform/security.tf
+++ terraform/security.tf
@@ -18,13 +18,13 @@ cidr_blocks = ["0.0.0.0/0"]
 ingress {
 from_port = 80
 to_port = 80
-protocol = "http"
+protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
 egress {
 from_port = 80
 to_port = 80
-protocol = "http"
+protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"]
 }
 ingress {
